ef3fff2074ae9bed0aedf877471ee55a8726d389
karn <mail@karngyan.com>
2024-10-21 14:06:02 +0530

    a decent amount of plugins

--- nvim/lua/karn/lazy.lua
+++ nvim/lua/karn/lazy.lua
@@ -11,5 +11,14 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("karn.plugins")
+require("lazy").setup({ { import = "karn.plugins" }, { import = "karn.plugins.lsp"} }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false
+  },
+})
+
 

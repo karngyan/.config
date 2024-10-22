7b394567ba84019bc1a4d2ab6cb303df11c06c0a
Karn <karn@customer.io>
2024-10-22 21:35:06 +0530

    update_focused_file

--- nvim/lua/karn/plugins/nvim-tree.lua
+++ nvim/lua/karn/plugins/nvim-tree.lua
@@ -1,8 +1,8 @@
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = "nvim-tree/nvim-web-devicons",
-  config = function() 
-        local nvimtree = require("nvim-tree")
+  config = function()
+    local nvimtree = require("nvim-tree")
 
     -- recommended settings from nvim-tree documentation
     vim.g.loaded_netrw = 1
@@ -26,6 +26,9 @@ return {
             },
           },
         },
+      },
+      update_focused_file = {
+        enable = true,
       },
       -- disable window_picker for
       -- explorer to work well with
@@ -52,5 +55,5 @@ return {
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
 }

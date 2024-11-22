d7cae846888416ba6ff7b2aef2185475294be773
Karn <karn@customer.io>
2024-11-22 16:50:52 +0530

    add live grep w args

--- nvim/lua/karn/plugins/telescope.lua
+++ nvim/lua/karn/plugins/telescope.lua
@@ -6,6 +6,12 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    },
   },
   config = function()
     local telescope = require("telescope")
@@ -34,6 +40,7 @@ return {
     })
 
     telescope.load_extension("fzf")
+    telescope.load_extension("live_grep_args")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
@@ -43,5 +50,6 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos " })
+    keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
   end,
 }
